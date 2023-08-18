360c6207d29ed064118d9596e66e807a05b94f43
Alex Humphreys <alex.humphreys@pleo.io>
2023-08-18 22:36:53 +0200

    Update kafka-tools

--- install-kafka-tools.sh
+++ install-kafka-tools.sh
@@ -4,14 +4,14 @@ set -e
 set -u
 
 INSTALL_DIR=${1:-/opt/kafka}
-KAFKA_SHA256="2d73625aeddd827c9e92eefb3c727a78455725fbca4361c221eaa05ae1fab02d"
-KAFKA_URL="https://archive.apache.org/dist/kafka/0.10.0.1/kafka_2.11-0.10.0.1.tgz"
+KAFKA_SHA256="18ad8a365fb111de249d3bb8bf3c96cd1af060ec8fb3e3d1fc4a7ae10d9042de"
+KAFKA_URL="https://archive.apache.org/dist/kafka/3.3.1/kafka_2.13-3.3.1.tgz"
 
-curl -L $KAFKA_URL -o kafka.tgz
+curl -L "$KAFKA_URL" -o kafka.tgz
 
 sha256sum -c <(echo "$KAFKA_SHA256 kafka.tgz")
 
-rm -rf $INSTALL_DIR
-mkdir $INSTALL_DIR
+rm -rf "$INSTALL_DIR"
+mkdir "$INSTALL_DIR"
 
-tar xf kafka.tgz -C $INSTALL_DIR --strip-components=1
+tar xf kafka.tgz -C "$INSTALL_DIR" --strip-components=1
